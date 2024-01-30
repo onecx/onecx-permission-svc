f1775f8fea0ba14f65853c634af116f98c9a3603
Andrej Petras <andrej@lorislab.org>
2024-01-30 16:48:10 +0100

    feat: add application info

---
+++ src/test/java/io/github/onecx/permission/rs/internal/controllers/ApplicationRestControllerTestIT.java
@@ -0,0 +1,8 @@
+package io.github.onecx.permission.rs.internal.controllers;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ApplicationRestControllerTestIT extends ApplicationRestControllerTest {
+
+}
